3cc8b676d09c20a594c918b7b4d68c6e48bfd5f4
Kartik <79395668+kartik11-dotcom@users.noreply.github.com>
2022-04-08 16:52:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('https://kartik11-dotcom.github.io/JB/sw.js')
     .then((reg) => console.log('service worker registered', reg))
     .catch((err) => console.log('service worker not registered', err))
 }
